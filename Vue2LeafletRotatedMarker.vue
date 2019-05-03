207590a32cf74e05ae30a8e26fea45e71b7e0032
Theme <Themex@users.noreply.github.com>
2019-05-03 13:20:17 +0300

    Update Vue2LeafletRotatedMarker.vue

--- Vue2LeafletRotatedMarker.vue
+++ Vue2LeafletRotatedMarker.vue
@@ -1,112 +1,112 @@
 <template>
-  <div style="display: none;">
-    <slot v-if="ready"></slot>
-  </div>
+    <div style="display: none;">
+        <slot v-if="ready"></slot>
+    </div>
 </template>
 
 <script>
-import L from 'leaflet'
-import 'leaflet-rotatedmarker';
-import { findRealParent, propsBinder } from 'vue2-leaflet';
+import { Icon, marker, DomEvent } from 'leaflet'
+import 'leaflet-rotatedmarker'
+import { findRealParent, propsBinder } from 'vue2-leaflet'
 
 const props = {
   draggable: {
     type: Boolean,
     custom: true,
-    default: false,
+    default: false
   },
   visible: {
     type: Boolean,
     custom: true,
-    default: true,
+    default: true
   },
   latLng: {
     type: [Object, Array],
-    custom: true,
+    custom: true
   },
   icon: {
     custom: false,
-    default: () => new L.Icon.Default(),
+    default: () => new Icon.Default()
   },
   zIndexOffset: {
     type: Number,
-    custom: false,
+    custom: false
   },
-  rotationAngle:{
+  rotationAngle: {
     type: Number,
-    default: () => 0,
+    default: () => 0
   },
   options: {
     type: Object,
-    default: () => ({}),
-  },
-};
+    default: () => ({})
+  }
+}
 export default {
   name: 'LRotatedMarker',
   props: props,
-  data() {
+  data () {
     return {
-      ready: false,
+      ready: false
     }
   },
-  mounted() {
-    const options = this.options;
+  mounted () {
+    const options = this.options
     if (this.icon) {
-      options.icon = this.icon;
+      options.icon = this.icon
     }
-    options.draggable = this.draggable;
-    options.rotationAngle = this.rotationAngle?this.rotationAngle:0;
-    this.mapObject = L.marker(this.latLng, options);
+    options.draggable = this.draggable
+    options.rotationAngle = this.rotationAngle ? this.rotationAngle : 0
+    this.mapObject = marker(this.latLng, options)
     this.mapObject.on('move', (ev) => {
       if (Array.isArray(this.latLng)) {
-        this.latLng[0] = ev.latlng.lat;
-        this.latLng[1] = ev.latlng.lng;
+        this.latLng[0] = ev.latlng.lat
+        this.latLng[1] = ev.latlng.lng
       } else {
-        this.latLng.lat = ev.latlng.lat;
-        this.latLng.lng = ev.latlng.lng;
+        this.latLng.lat = ev.latlng.lat
+        this.latLng.lng = ev.latlng.lng
       }
-    });
-    L.DomEvent.on(this.mapObject, this.$listeners);
-    propsBinder(this, this.mapObject, props);
-    this.ready = true;
-    this.parentContainer = findRealParent(this.$parent);
-    this.parentContainer.addLayer(this, !this.visible);
+    })
+    DomEvent.on(this.mapObject, this.$listeners)
+    propsBinder(this, this.mapObject, props)
+    this.ready = true
+    this.parentContainer = findRealParent(this.$parent)
+    this.parentContainer.addLayer(this, !this.visible)
   },
-  beforeDestroy() {
-    this.parentContainer.removeLayer(this);
+  beforeDestroy () {
+    this.parentContainer.removeLayer(this)
   },
   methods: {
-    setDraggable(newVal, oldVal) {
+    setDraggable (newVal, oldVal) {
       if (this.mapObject.dragging) {
-        newVal ? this.mapObject.dragging.enable() : this.mapObject.dragging.disable();
+        newVal ? this.mapObject.dragging.enable() : this.mapObject.dragging.disable()
       }
     },
-    setVisible(newVal, oldVal) {
-      if (newVal == oldVal) return;
+    setVisible (newVal, oldVal) {
+      if (newVal === oldVal) return
       if (this.mapObject) {
         if (newVal) {
-          this.parentContainer.addLayer(this);
+          this.parentContainer.addLayer(this)
         } else {
-          this.parentContainer.removeLayer(this);
+          this.parentContainer.removeLayer(this)
         }
       }
     },
-    setLatLng(newVal) {
+    setLatLng (newVal) {
       if (newVal == null) {
-        return;
+        return
       }
       if (this.mapObject) {
-        let oldLatLng = this.mapObject.getLatLng();
+        let oldLatLng = this.mapObject.getLatLng()
         let newLatLng = {
           lat: newVal[0] || newVal.lat,
-          lng: newVal[1] || newVal.lng,
-        };
-        if (newLatLng.lat != oldLatLng.lat || newLatLng.lng != oldLatLng.lng) {
-          this.mapObject.setLatLng(newLatLng);
+          lng: newVal[1] || newVal.lng
+        }
+        if (newLatLng.lat !== oldLatLng.lat || newLatLng.lng !== oldLatLng.lng) {
+          this.mapObject.setLatLng(newLatLng)
         }
       }
     }
   }
-};
+}
 
 </script>
